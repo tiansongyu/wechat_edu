f21779c922b44ca3816634404402137953881220
tiansongyu <478880214@qq.com>
2026-07-16 08:23:50 +0800

    fix(mini): restore ngrok endpoint for device testing

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-// 服务器 Docker 网关地址；正式体验版应替换为映射到该网关的 HTTPS 合法域名。
-const API_BASE_URL = "http://89.117.20.124:4000";
+// 真机测试通过 HTTPS ngrok 隧道访问服务器 Docker 网关。
+const API_BASE_URL = "https://tablet-litter-rocker.ngrok-free.dev";
 
 module.exports = { API_BASE_URL };
